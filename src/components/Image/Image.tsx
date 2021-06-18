1bd0d2cfecd3e8af0b7182967418742504585ca6
Nils Schönwald <nils@schoen.world>
2021-06-18 02:31:23 +0200

    remove contentful loader again

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,17 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 import NextImage, { ImageProps } from 'next/image'
-import qs from 'qs'
 import { FC } from 'react'
-
-const contentfulLoader = ({ src, width, quality }) => {
-  const params = {
-    fm: 'webp', // format
-    w: width || null,
-    q: quality || 75,
-  }
-
-  return `https:${src}?${qs.stringify(params, { skipNulls: true })}`
-}
 
 type ImageType =
   | (ImageProps & { isExternal?: never })
@@ -22,16 +11,17 @@ type ImageType =
 
 export const Image: FC<ImageType> = ({ isExternal, ...props }) => {
   if (isExternal) {
-    // eslint-disable-next-line @next/next/no-img-element
-    return <img {...props} src={props.src as string} loading="lazy" />
+    return (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img {...props} src={props.src as string} loading="lazy" />
+    )
   }
 
   const isContentful = (props.src as string).includes('ctfassets')
 
-  return (
-    <NextImage
-      loader={isContentful ? contentfulLoader : undefined}
-      {...props}
-    />
-  )
+  if (isContentful) {
+    props.src = `https:${props.src}` as typeof props.src
+  }
+
+  return <NextImage {...props} />
 }
